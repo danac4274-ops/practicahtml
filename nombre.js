0c14fcfcbcb6c67aa86d8593723217e467520660
danac4274-ops <danac4274@gmail.com>
2025-11-28 16:02:18 -0600

    ciclos

--- nombre.js
+++ nombre.js
@@ -57,3 +57,68 @@ console.log(moto);
 
 console.log(moto.marca)
 console.log(moto.motor.tipo)
+
+
+for(let i =1; i <= 10; i++){
+    console.log(i)
+}
+
+for(let i =100; i >=1; i--){
+    console.log(i)
+}
+function sumar(a, b){
+    return a+b;
+}
+
+console.log(sumar(7,9))
+
+function multiplicar(c, d){
+    return c*d
+}
+
+console.log(multiplicar(5,6))
+
+let numeros =[5, 10, 15, 20, 25];
+let suma3 =0;
+
+for(let i = 0; i<numeros.length; i++){
+    suma3 += numeros [i];
+
+}
+
+console.log("la suma es:", suma3);
+
+let libros =[10, 15, 20, 50, 80, 10];
+let suma4 =0;
+
+for(let i = 0; i<libros.length; i++){
+    suma4 += libros [i];
+}
+
+console.log("la suma es:", suma4)
+
+function sumararreglo(arr){
+    let sumaarr = 0;
+
+    for(let i = 0; i < arr.length; i++){
+        suma += arr[i];
+    }
+    return suma;
+}
+
+console.log(sumararreglo([10,20,30]));
+
+
+function restararreglo(arr){
+    let resta = 0;
+
+    for(let i = 0; i < arr.length; i++){
+       resta -= arr[i];
+    }
+     return resta;
+}
+
+console.log(restararreglo([100,50,20]));
+
+
+
